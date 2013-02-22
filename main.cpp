327a6d2e26a4b21474c55e952bc18a5bdf05acde
Marcelo <public.mranzano@gmail.com>
2013-02-23 06:19:48 +0800

    Final ng Prob 11-20 (suko na kami)

--- main.cpp
+++ main.cpp
@@ -1,79 +1,116 @@
+#include "project.h"
 #include <iostream>
 #include <string>
 #include <cstdlib>
-#include "project.h"
 
 using namespace std;
 
-const string Problem[]={"Multiples of 3 and 5",
-                        "Even Fibonacci numbers",
-                        "Largest prime factor",
-                        "Largest palindrome product",
-                        "Smallest multiple",
-                        "Sum square difference",
-                        "10001st prime",
-                        "Largest product in a series",
-                        "Special Pythagorean triplet",
-                        "Summation of primes","Exit"};
+const string problem[]={
+        "Multiples of 3 and 5",
+        "Even Fibonacci Numbers",
+        "Largest Prime Factor",
+        "Largest Palindrome Product",
+        "Smallest Multiple",
+        "Sum Square Difference",
+        "10001st Prime",
+        "Largest Product in a Series",
+        "Special Pythagorean Triplet",
+        "Summation of Primes",
+        "Largest Product in a Grid",
+        "Highly Divisible Triangular Number",
+        "Large Sum",
+        "Longest Collatz Sequence",
+        "Diophantine Reciprocals A",
+        "Power Digit Sum",
+        "Number Letter Counts",
+        "Maximum Path Sum [Case I]",
+        "Counting Sundays",
+        "Factorial Digit Sum",
+        "Exit"
+    };
 
-const int NUM_PROBLEMS=10;
+const int NUM_PROBLEMS=20;
 const int EXIT=NUM_PROBLEMS+1;
 
 int main()
 {
+    bool done=false;
+    int i=0,ch=0;
     project proj;
-    bool done = false;
-    int i=0, ch=0;
-
     while (done==false)
     {
         system("cls");
-        for(i=0; i<(NUM_PROBLEMS+1);i++)
-           {
-               cout<<(i+1)<<". "<<Problem[i]<<endl;
-           }
-
-        cout<<"choose problem: ";
+        for (i=0;i<(NUM_PROBLEMS+1);i++)
+            cout<<(i+1)<<"."<<problem[i]<<endl;
+        cout<<"\nChoose Problem: ";
         cin>>ch;
 
         switch(ch)
-            {
-                case 1:
-                    proj.multiples_of_3_and_5();
-                    break;
-                case 2:
-                    proj.even_fibonacci();
-                    break;
-                case 3:
-                    proj.largest_prime_factor();
-                    break;
-                case 4:
-                    proj.largest_palindrome_product();
-                    break;
-                case 5:
-                    proj.smallest_multiple();
-                    break;
-                case 6:
-                    proj.sum_square_diff();
-                    break;
-                case 7:
-                    proj._10001st_prime();
-                    break;
-                case 8:
-                    proj.largest_product_in_series();
-                    break;
-                case 9:
-                    proj.special_pytho_triplet();
-                    break;
-                case 10:
-                    proj.summation_of_primes();
-                    break;
-                case EXIT:
-                    done=true;
-                    break;
-            }
+        {
+            case 1:
+                proj.multiples_of_3_and_5();
+                break;
+            case 2:
+                proj.even_fibonacci();
+                break;
+            case 3:
+                proj.largest_prime_factor();
+                break;
+            case 4:
+                proj.largest_palindrome_product();
+                break;
+            case 5:
+                proj.smallest_multiple();
+                break;
+            case 6:
+                proj.sum_square_diff();
+                break;
+            case 7:
+                proj._10001st_prime();
+                break;
+            case 8:
+                proj.largest_product_in_series();
+                break;
+            case 9:
+                proj.special_pytho_triplet();
+                break;
+            case 10:
+                proj.summation_of_primes();
+                break;
+            case 11:
+                proj.largest_product_grid();
+                break;
+            case 12:
+                proj.divisible_triangular_number();
+                break;
+            case 13:
+                proj.largest_sum();
+                break;
+            case 14:
+                proj.longest_collatz_sequence();
+                break;
+            case 15:
+               proj.diophantine_reciprocals_a();
+                break;
+            case 16:
+                proj.power_digit_sum();
+                break;
+            case 17:
+                proj.number_letter_count();
+                break;
+            case 18:
+                proj.max_path_sum_one();
+                break;
+            case 19:
+                proj.counting_sundays();
+                break;
+            case 20:
+                proj.factorial_digit_sum();
+                break;
+            case EXIT:
+                done=true;
+                break;
+        }
         system("pause");
     }
-
-return 0;
 }
--- project.h
+++ project.h
@@ -1,25 +1,41 @@
 #ifndef PROJECT_H
 #define PROJECT_H
+#include <string>
 
+using namespace std;
 
 class project
 {
     public:
         project();
         virtual ~project();
-        bool isPrime(int);
-        void multiples_of_3_and_5();
-        void even_fibonacci();
-        void largest_prime_factor();
-        void largest_palindrome_product();
-        void smallest_multiple();
-        void sum_square_diff();
-        void _10001st_prime();
-        void largest_product_in_series();
-        void special_pytho_triplet();
-        void summation_of_primes();
+
+        void multiples_of_3_and_5(); //problem 1
+        void even_fibonacci(); //problem 2
+        void largest_prime_factor(); //problem 3
+        void largest_palindrome_product(); //problem 4
+        void smallest_multiple(); //problem 5
+        void sum_square_diff(); //problem 6
+        void _10001st_prime(); //problem 7
+        void largest_product_in_series(); //problem 8
+        void special_pytho_triplet(); //problem 9
+        void summation_of_primes(); //problem 10
+        void largest_product_grid();//problem 11
+        void divisible_triangular_number(); //problem 12
+        void largest_sum(); //problem 13
+        void longest_collatz_sequence(); //problem 14
+        void diophantine_reciprocals_a(); //problem 15
+        void power_digit_sum(); //problem 16
+        void number_letter_count(); //problem 17
+        void max_path_sum_one(); //problem 18
+        void counting_sundays(); //problem 19
+        void factorial_digit_sum();//Problem 20
     protected:
     private:
+        bool isPrime(int);
+        int countFactors(int);
+        void seive(int n);
+        int sumDigits(string);
 };
 
 #endif // PROJECT_H
