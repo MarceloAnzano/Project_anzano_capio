3942a4d128fccd7573c7b6a2159bbee87f6ee63d
Marcelo Anzano <public.mranzano@gmail.com>
2013-03-23 04:19:15 +0800

    w/ BigInt

--- project.cpp
+++ project.cpp
@@ -16,6 +16,7 @@ vector <int> primes;
 #define LIMIT	1000
 #define sz 1000001
 #define mx 1001
+#include "BigInteger/BigIntegerLibrary.hh"
 
 using namespace std;
 
@@ -660,38 +661,19 @@ void project::diophantine_reciprocals_a()
 
 void project::power_digit_sum()
 {
-    vector<int> digits ;
-	digits.push_back( 1 );
-	for( int i=1; i<=1000; i++)
-	{
-		int carryover = 0;
-		for ( vector<int>::iterator iter = digits.begin(); iter != digits.end(); iter++ )
-		{
-			*iter = *iter * 2 + carryover;
-			if ( *iter > 9 && (iter+1) != digits.end() )
-			{
-				*iter -= 10;
-				carryover = 1;
-			}
-			else if ( *iter > 9 && (iter+1) == digits.end())
-			{
-				*iter -= 10;
-				digits.push_back(1);
-				break;
-			}
-			else if ( *iter <= 9 )
-				carryover = 0;
-		}
-	}
-
-	int sum = 0;
-
-	for ( vector<int>::iterator iter = digits.begin(); iter != digits.end(); iter++ )
-		sum += *iter;
+    BigInteger power=1;
+    string s;
+    int ctr=0;
+    while(ctr<=999)
+    {
+        power=power*2;
+        ctr++;
+    }
+    s = bigIntegerToString(power);
 
 	cout<<"-------------------------------------------------------------------------------"<<endl;
 	cout<<"The sum of the digits of the number 21000 is: ";
-	cout <<sum<<endl;
+	cout <<sumDigits(s)<<endl;
     cout<<"-------------------------------------------------------------------------------"<<endl;
 }
 
@@ -1005,7 +987,18 @@ void project::counting_sundays()
 
 void project::factorial_digit_sum()
 {
-    string s = "93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000";
+
+    BigInteger factorial = 1;
+    int n=100;
+    string s;
+
+    while(n!=0)
+    {
+        factorial  = factorial*n;
+        n--;
+    }
+    s = bigIntegerToString(factorial);
+
     cout<<"-------------------------------------------------------------------------------"<<endl;
     cout<<"The Factorial Digit Sum is: ";
     cout<<sumDigits(s) << endl;
